5cbad996709708607aff48b5ea567748d2b4e70c
anitaparmar26 <48512144+anitaparmar26@users.noreply.github.com>
2021-08-21 14:54:41 +0530

    removed sidebar and dropdown function

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -50,36 +50,6 @@
   }
 
 
-  // Multi level menu dropdown
-
-  if ($(".dropdown-menu a.dropdown-toggle").length) {
-    $(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
-      if (!$(this)
-        .next()
-        .hasClass("show")
-      ) {
-        $(this)
-          .parents(".dropdown-menu")
-          .first()
-          .find(".show")
-          .removeClass("show");
-      }
-      var $subMenu = $(this).next(".dropdown-menu");
-      $subMenu.toggleClass("show");
-
-      $(this)
-        .parents("li.nav-item.dropdown.show")
-        .on("hidden.bs.dropdown", function (e) {
-          $(".dropdown-submenu .show").removeClass("show");
-        });
-
-      return false;
-    });
-  }
-
-
-
-
 
   // Default Tooltip
 
@@ -89,10 +59,6 @@
       return new bootstrap.Tooltip(tooltipTriggerEl)
     })
   }
-
-
-
-
 
 
   // Default Popover
@@ -177,53 +143,6 @@ var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
 })
 
   }
-
-
-
-
-
-  // Sidenav fixed for docs
-
-  if ($(".sidebar-nav-fixed a").length) {
-    $(".sidebar-nav-fixed a")
-      // Remove links that don't actually link to anything
-      .on("click", function (event) {
-        // On-page links
-        if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-          location.hostname == this.hostname
-        ) {
-          // Figure out element to scroll to
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          // Does a scroll target exist?
-          if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: target.offset().top - 90
-            }, 1000, function () {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
-            });
-          }
-        };
-        $('.sidebar-nav-fixed a').each(function () {
-          $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      });
-  }
-
-
 
 
 
